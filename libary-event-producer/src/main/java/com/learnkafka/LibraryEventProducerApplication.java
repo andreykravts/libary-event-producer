cb6f1614487098f62dbaa01b51ac1bee741c7271
andreykravts <andreykravts2210@gmail.com>
2024-02-21 12:45:56 +0200

    Update

--- libary-event-producer/src/main/java/com/learnkafka/LibraryEventProducerApplication.java
+++ libary-event-producer/src/main/java/com/learnkafka/LibraryEventProducerApplication.java
@@ -2,13 +2,12 @@ package com.learnkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 
 @SpringBootApplication
-public class LibaryEventProducerApplication {
+public class LibraryEventProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibaryEventProducerApplication.class, args);
+		SpringApplication.run(LibraryEventProducerApplication.class, args);
 	}
 
 }
